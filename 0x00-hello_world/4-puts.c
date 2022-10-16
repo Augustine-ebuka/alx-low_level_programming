812b531747eda2a0ded77cee767ea2b17b670465
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2022-10-16 04:06:30 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main- serves as the entry point for the program
- *
- * Return: By default returns zero if no errors otherwise return non -zero value
- * */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
