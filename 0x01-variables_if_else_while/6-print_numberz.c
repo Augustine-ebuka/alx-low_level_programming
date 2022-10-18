1d3889bf1c413170869f6dff341c0c71e7c2c8e0
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2022-10-18 01:01:25 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** this program prints all single digits
+* from 1-10
+*/
+int main()
+{
+  int i;
+  for (i = 0; i <= 10; i++)
+  {
+    putchar(i);
+  }
+  putchar("\n");
+  return (0)
+}
