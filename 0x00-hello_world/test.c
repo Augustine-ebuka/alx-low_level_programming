291dffbd2cca09acf31cdb6d317229d813027e92
Augustine-ebuka <augustineebuka98@gmail.com>
2022-10-20 02:56:46 -0700

    debug task

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** this just print random
+ *
+ */
+int main(void)
+{
+	printf("he size of int = %d", sizeof(int));
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this project teaches on how to debugg, more details coming
