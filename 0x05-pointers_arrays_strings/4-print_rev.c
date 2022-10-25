1468811563828010bad9fc52428f0b3c0ea1e668
Augustine-ebuka <augustineebuka98@gmail.com>
2022-10-24 21:54:10 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print revers of a string
+ * @s: the pointer to the string to be reversed
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	while (s[i] = '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	int k;
+	for (k = j; k >= 0; k--)
+	{
+		-putchar(s[k]);
+	}
+	_putchar('\n');
+}
