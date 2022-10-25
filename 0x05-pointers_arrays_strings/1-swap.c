154d957ce14c1bcda4b3911e44e8a8dcf89055c0
Augustine-ebuka <augustineebuka98@gmail.com>
2022-10-24 20:45:26 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a : pointer to the first value
+ * @b : pointer t the second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = *a;
+
+}
