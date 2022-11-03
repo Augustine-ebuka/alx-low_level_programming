fdd8619bcb553bf15aa5126f721a81b5405d822a
Augustine-ebuka <augustineebuka98@gmail.com>
2022-11-03 09:21:22 -0700

    my first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,6 +8,15 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s);
-	_putchar("\n");
+	int i;
+	i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
 }
