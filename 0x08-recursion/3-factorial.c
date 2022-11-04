679c2880df661469809a4b6a2e9d209e4c6eb7f8
Augustine-ebuka <augustineebuka98@gmail.com>
2022-11-04 07:32:31 -0700

    my first commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * factorial - the function return factorial
+ * @n: num to find its factorial
+ *
+ * Return: return the factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
