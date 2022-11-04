7c838110f7d0ba9711ed94fd2d414afe0112a715
Augustine-ebuka <augustineebuka98@gmail.com>
2022-11-03 19:41:44 -0700

    my first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ *  _strlen_recursion - a function thst returnsnthe length of a strng
+ *  @s: a pointer to a strng variable
+ *
+ *  Return: the legnth of s
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return(0);
+	}
+	return (i++);
+	_strlen_recursion(s + 1)
+}
