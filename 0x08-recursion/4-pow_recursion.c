933b7f4e89ebd0dc19acaededef8b2fccd25d975
Augustine-ebuka <augustineebuka98@gmail.com>
2022-11-05 15:02:29 -0700

    my first commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  f(n) that returns the value of x raised to the power of y
+ * @x: a number to be raised to the power of y
+ * @y: a power of x
+ *
+ * Return: return the result of x to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
