af48cf0501ff041e8ce5a03e0b40d29ef4fba2a7
Augustine-ebuka <augustineebuka98@gmail.com>
2022-11-07 12:35:15 -0800

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success
+ */
+int main(int argc __attribute((unused)), char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
